90578b4adc9486ff3b8aed947e41cfbd4cad03c8
Sudharsan <sudharsan.0125@gmail.com>
2022-02-22 20:53:53 +0530

    Adding ModelForm to the Project

--- projects/views.py
+++ projects/views.py
@@ -1,25 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.test import tag
 from .models import Project
+from .forms import ProjectForm
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+
 
 # Create your views here.
 def projects(request):
@@ -32,3 +17,22 @@ def project(request,pk):
     tags = projectObj.tags.all()
     context = {"project":projectObj, "tags":tags}
     return render(request, 'projects/single-project.html', context)
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
+    context = {"form":form}
+    return render(request, "projects/project_form.html",context)
+
+def updateProject(request,pk):
+    form = ProjectForm()
+
+    context = {"form":form}
+
+    return render(request, "projects/project_form.html",context)
